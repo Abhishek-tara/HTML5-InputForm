dd0df43be67d5f18f5aea4e32bef996d399d4506
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-30 14:15:05 +0530

    Added Ability to check Email

--- form.js
+++ form.js
@@ -20,7 +20,7 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('.email-error');
 email.addEventListener('input', function () {
     let emailRegex = RegExp('^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$');
-    if (emailRegex.test(text.value))
+    if (emailRegex.test(email.value))
     emailError.textContent = ""
     else
     emailError.textContent = "Email is Incorrect";
