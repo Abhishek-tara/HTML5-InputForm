9379d6c8a83808efd03c235ae193483dd90354d7
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-30 14:12:57 +0530

    Added Ability to check password with all Rules

--- form.js
+++ form.js
@@ -37,11 +37,11 @@ number.addEventListener('input', function () {
     numError.textContent = "Number is Incorrect";
 });
 
-//UC4
+//UC4,5
 const password = document.querySelector('#pwd');
 const pwdError = document.querySelector('.password-error');
 password.addEventListener('input', function () {
-    let pwdRegex = RegExp("^[0-9A-Za-z!@#$%^&*?<>]{8,}$");
+    let pwdRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$");
     if (pwdRegex.test(password.value))
     pwdError.textContent = ""
     else
