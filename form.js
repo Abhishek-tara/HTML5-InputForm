cae81e9f673ab717e2dc9348ea81b3ef28f0f22b
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-30 14:03:59 +0530

    Added Ability to check phone Number

--- form.js
+++ form.js
@@ -20,8 +20,19 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('.email-error');
 email.addEventListener('input', function () {
     let emailRegex = RegExp('^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$');
-    if (emailRegex.test(text.value))
+    if (emailRegex.test(email.value))
     emailError.textContent = ""
     else
     emailError.textContent = "Email is Incorrect";
+});
+
+//UC3
+const number = document.querySelector('#contact');
+const numError = document.querySelector('.number-error');
+number.addEventListener('input', function () {
+    let numRegex = RegExp("^[0-9]{10}$");
+    if (numRegex.test(number.value))
+    numError.textContent = ""
+    else
+    numError.textContent = "Number is Incorrect";
 });
